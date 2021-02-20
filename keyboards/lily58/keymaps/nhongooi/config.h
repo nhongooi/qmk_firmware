5174d32cdaf867aae15ef3b930acacc01acc5e93
Tim Nguyen <tug56656@temple.edu>
2021-02-20 12:20:32 -0500

    rev 2

--- keyboards/lily58/keymaps/nhongooi/config.h
+++ keyboards/lily58/keymaps/nhongooi/config.h
@@ -42,11 +42,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
-
-// Underglow
-/*
-#undef RGBLED_NUM
-#define RGBLED_NUM 14    // Number of LEDs
-#define RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_SLEEP
-*/
+#define OLED_TIMEOUT 120000
+#define OLED_BRIGHTNESS 120
--- keyboards/lily58/keymaps/nhongooi/rules.mk
+++ keyboards/lily58/keymaps/nhongooi/rules.mk
@@ -1,7 +1,3 @@
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
-#
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
@@ -15,10 +11,8 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_DRIVER_ENABLE= no     # OLED display
-
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+OLED_DRIVER_ENABLE= yes     # OLED display
+WPM_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
